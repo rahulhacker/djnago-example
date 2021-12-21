46aa6ad38b1a6bde3737cb60e5b5a2c0414e7631
Rahul Kumar <rahulkumar.10.06.1991@gmail.com>
2021-12-21 11:31:52 +0530

    Added url rerouting from html

--- queries/views.py
+++ queries/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'queries/index.html')
+
+def add(request):
+    return render(request, 'queries/add.html')
